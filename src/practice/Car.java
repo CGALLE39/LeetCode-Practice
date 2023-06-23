2f0243b75a588f4481f7e5838a3eef45c6739e18
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-06-23 16:51:00 -0500

    calculator frame and button setup

---
+++ src/practice/Car.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Car {
+
+    String make ="Tesla";
+    private String model = "Model X";
+    String color = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public void describeCar() {
+        System.out.println(doors + "-Door" +
+                color + " " +
+                make + " " +
+                model + " " +
+                        (convertible ? "Convertible" : "")
+                );
+    }
+}
---
+++ src/practice/Main.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.make = "Porsche";
+        car.color ="Red";
+        car.describeCar();
+    }
+}
