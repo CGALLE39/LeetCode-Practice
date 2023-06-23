cb3e53b3fdeddd1051b0fbe24b45c68fe4b6e0d2
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-06-23 17:23:40 -0500

    java calculator for fun

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -10,9 +10,9 @@ public class Calculator implements ActionListener{
     JFrame frame;
     JTextField textfield;
     JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[8];
+    JButton[] functionButtons = new JButton[9];
     JButton addButton,subButton,mulButton,divButton;
-    JButton decButton, equButton, delButton, clrButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
 
     Font myFont = new Font("Ink Free", Font.BOLD,30);
@@ -112,9 +112,73 @@ public class Calculator implements ActionListener{
         Calculator cal = new Calculator();
     }
 
+    /* functionality*/
     @Override
     public void actionPerformed(ActionEvent e) {
 
-    }
+        for(int i = 0; i < 10; i ++) {
+            if(e.getSource() == numberButtons[i]) {
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource()==decButton) {
+            textfield.setText(textfield.getText().concat("."));
+        }
+        if (e.getSource()==addButton) {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '+';
+            textfield.setText("");
+        }
+        if (e.getSource()==subButton) {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '-';
+            textfield.setText("");
+        }
+        if (e.getSource()==mulButton) {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '*';
+            textfield.setText("");
+        }
+        if (e.getSource()==divButton) {
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '/';
+            textfield.setText("");
+        }
+        if (e.getSource()==equButton) {
+            num2 = Double.parseDouble(textfield.getText());
 
+            switch (operator) {
+                case '+' :
+                    result = num1 + num2;
+                    break;
+
+                case '-' :
+                    result = num1 - num2;
+                    break;
+
+                case '*' :
+                result = num1 * num2;
+                    break;
+
+                case '/' :
+                    result = num1 / num2;
+                    break;
+            }
+
+            textfield.setText(String.valueOf(result));
+            num1 = result;
+        }
+
+        if (e.getSource()==clrButton) {
+            textfield.setText("");
+        }
+        if (e.getSource()==delButton) {
+           String string = textfield.getText();
+            textfield.setText("");
+            for (int i = 0; i < string.length()-1; i++) {
+                textfield.setText(textfield.getText()+string.charAt(i));
+
+            }
+        }
+    }
 }
