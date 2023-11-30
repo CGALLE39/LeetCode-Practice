0a3d7229a983c6ee1e671b3bd3b34fbbaf8790c1
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-11-30 12:35:56 -0600

    class

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car car = new Car();
-        car.make = "Porsche";
-        car.color ="Red";
-        car.describeCar();
+//        Car car = new Car();
+//        car.make = "Porsche";
+//        car.color ="Red";
+//        car.describeCar();
+
+        Cat catOne = new Cat("orange");
+
+        System.out.println(catOne.getColor());
     }
 }
